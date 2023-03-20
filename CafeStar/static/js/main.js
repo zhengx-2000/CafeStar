21a13f2e9579cea5f8b0d2397c99af276c923eba
ziyizhang <2743774z@student.gla.ac.uk>
2023-03-20 21:00:07 +0000

    fix file path

---
+++ CafeStar/static/js/main.js
@@ -0,0 +1,14 @@
+
+/*Drinks*/
+$(document).ready(function(){
+    $("#drinkLinkBtn").click(function() {
+        location.href = "drinks.html";
+    });
+});
+
+/*homePage*/
+$(document).ready(function(){
+    $("#homePageBtn").click(function() {
+        location.href = "homePage.html";
+    });
+});
