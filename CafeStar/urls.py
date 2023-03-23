bf2f871078b30c87963e755e5d7382f37422ab45
Xiao Zheng <2773671z@student.gla.ac.uk>
2023-03-23 03:22:17 +0000

    Added some urls

--- CafeStar/urls.py
+++ CafeStar/urls.py
@@ -5,6 +5,7 @@ app_name = 'CafeStar'
 
 urlpatterns = [
     path('', views.homePage, name='home_page'),
+    path('homePage', views.homePage, name='home_page'),
     path('drinkDetail', views.drinkDetail, name='drink_detail'),
     path('drinks', views.drinks, name='drinks'),
     path('order', views.order, name='order'),
@@ -13,4 +14,7 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('edit', views.userProfile, name='edit'),
+    path('orderList', views.orderList, name='order_list'),
+    path('shopStatus', views.status, name='shop_status'),
+    path('drinksModify', views.drinksModify, name='drinks_modify'),
 ]
